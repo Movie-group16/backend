92741f1d892401d6bef1b05ecf9d697b8ba5b2a9
N1ppexd <niilohanni@gmail.com>
2025-09-10 21:37:32 +0300

    requestit toimii

--- index.js
+++ index.js
@@ -24,8 +24,47 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+    const pool = openDb()
+
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).json(results.rows)
+    })
+
 })
+
+app.get('/groups', (req, res) => {
+
+    const pool = openDb()
+
+    pool.query('SELECT * FROM groups', (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).json(results.rows)
+    })
+
+})
+
+app.get('/groupUsers', (req, res) => {
+
+    const pool = openDb()
+
+    pool.query('SELECT username, email, user_desc, group_name, group_desc, group_rules FROM groupUser INNER JOIN users ON groupUser.user_id = users.id INNER JOIN groups ON groupUser.group_id = groups.id', (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(200).json(results.rows)
+    })
+
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
