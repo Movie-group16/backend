6f623424567bc35525597dd496e776c657af4c0c
N1ppexd <niilohanni@gmail.com>
2025-09-10 21:27:54 +0300

    I have init.sql and stuiff for testing

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from 'express'
 import cors from 'cors'
+import pkg from 'pg'
+import dotenv from 'dotenv'
 
 const port = process.env.PORT || 3001
+const { Pool } = pkg
+dotenv.config()
+
+
+const openDb = () => {
+    const pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+    })
+    return pool
+}
+
 
 const app = express()
 app.use(cors())
