8755f85655b44d516f7cd1f18a512d54c877182a
N1ppexd <niilohanni@gmail.com>
2025-10-04 15:02:09 +0300

    get review

--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -14,6 +14,19 @@ router.get('/', (req, res, next) => {
     })
 })
 
+router.get('/:userId'), (req, res, next) => {
+
+    const { user_id } = req.params
+
+    pool.query('SELECT * FROM reviews WHERE user_id = $1 RETURNING *', [user_id], (err, result) => {
+        if(err){
+            return next(err)
+        }
+
+        res.status(200).json(result.rows)
+    })
+}
+
 router.post('/', (req, res, next) => {
     const { review } = req.body
 
